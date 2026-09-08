263ee5d43872c5008dacc48cc83170f4a0527380
GemmandKilua2010 <specterxofc@outlook.com>
2026-09-08 16:45:16 -0300

    Update libraly.lua

--- libraly.lua
+++ libraly.lua
@@ -3707,7 +3707,7 @@ function redzlib:MakeWindow(Configs)
 			local Logo = Configs[2] or Configs.Logo or ""
 			local Invite = Configs[3] or Configs.Invite or ""
 
-			local RPC = loadstring(game:HttpGet("https://raw.githubusercontent.com/GemmandKilua2010/Scripts/refs/heads/main/Discord/Invite.lua")()
+			local RPC = loadstring(game:HttpGet("https://raw.githubusercontent.com/GemmandKilua2010/Scripts/heads/main/Discord/Invite.lua"))()
 			
 			local InviteHolder = Create("Frame", Container, {
 				Size = UDim2.new(1, 0, 0, 80),
